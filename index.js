0cf9808bbb53d7e7c2cc0ca56b8b0bacb8396bb7
pascal de creator <pascall.de.creator@gmail.com>
2022-07-02 09:45:21 +0000

    add timestamp feature

--- index.js
+++ index.js
@@ -8,25 +8,37 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", (req, res) => {
+  const givenDate = req.params.date
+  let date
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // check if no date provided
+  if (!givenDate) {
+    date = new Date()
+  } else {
+    // check if unix time:
+    const checkUnix = givenDate * 1
+    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix)
+  }
+
+  //check if valid format
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" })
+  } else {
+    const unix = date.getTime()
+    const utc = date.toUTCString()
+    res.json({ unix, utc })
+  }
 });
 
-
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
